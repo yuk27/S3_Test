08647331236a118e3b9b81405547200c32f0cae3
yuk2794@gmail.com <25yB$!E*@vhG&RDDto0>
2022-03-23 23:37:10 -0600

    Main commited

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
 import boto3
+import os
 
+try:
+    with open('new_user_credentials.csv', 'r') as file:
+        file.readline()
+        creds_parts = file.readline().split(",")
 
+    ACCESS_KEY = creds_parts[2]
+    SECRET_ACCESS_KEY = creds_parts[3]
+except:
+    ACCESS_KEY = os.environ.get("USER_CREDENTIALS_USR")
+    SECRET_ACCESS_KEY = os.environ.get("USER_CREDENTIALS_PSW")
 
-"""
-with open('new_user_credentials.csv', 'r') as file:
-    file.readline()
-    creds_parts = file.readline().split(",")
-
-ACCESS_KEY = creds_parts[2]
-SECRET_ACCESS_KEY = creds_parts[3]
 BUCKET_NAME = 'jsolano.jenkinstest.s3'
 
 s3 = boto3.client(
@@ -23,4 +26,3 @@ for file in bucket['Contents']:
     name = file['Key']
     print("Downloading file: {0}".format(name))
     s3.download_file(BUCKET_NAME, name, 'res/{0}'.format(name))
-"""
